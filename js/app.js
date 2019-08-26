e377558f3fa88fd81321bf0a367678f7cf93feb9
ofega <fegaeze@gmail.com>
2019-08-26 21:58:23 +0100

    fixed parallax scroll for new header height

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ const controller = new ScrollMagic.Controller();
 const timeline = new TimelineMax();
 
 timeline
-.to('#sixth', 6, {y: -550})
-.to('#fifth', 6, {y: -500}, '-=6')
+.to('#sixth', 6, {y: -450})
+.to('#fifth', 6, {y: -400}, '-=6')
 .to('#fourth', 6, {y: -400}, '-=6')
 .to('#third', 6, {y: -300}, '-=6')
 .to('#second', 6, {y: -200}, '-=6')
